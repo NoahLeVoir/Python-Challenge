703686ccdcf46edfdcdce13db07a894167e0423a
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-01-20 23:55:29 -0600

    PyPoll Update: Prep CSV file and check that it is being read properly (successful)

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,1 +1,21 @@
-Print("Hello from PyPoll!")
+# Import modules
+import os
+import csv
+
+# Set path for the csv file in the Resources folder
+csvpath = os.path.join("Resources", "election_data.csv")
+
+# PLACEHOLDER
+# Will Dim the variables here before the 'with' opens the csv
+
+# Open the CSV and make sure that it is being read
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # Print csvreader to make sure everything is set up correctly
+    print(csvreader)
+
+    # Recognize the header row, and know to skip when working with the rest of the rows
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
