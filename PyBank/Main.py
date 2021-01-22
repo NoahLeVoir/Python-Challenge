9a449bc10fdcf0c974c06f71a50e8a7a34695013
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-01-22 11:55:23 -0600

    PyBank Update: Cleaned up comments and minor formatting for the print results

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -5,13 +5,13 @@ import csv
 # Set path for the csv file in the Resources folder
 csvpath = os.path.join("Resources", "budget_data.csv")
 
-# Define variables that will be pulled from CSV
+# Initial define of variables that will be pulled from CSV
 total_months = 0
 net_profit = 0
 month = 0
 profit_loss = 0
 
-# Define lists hold the months and the profit/loss changes
+# Define lists that will hold the months and the profit/loss changes
 # This is a place to store the values as the for loop goes through the csv
 month_count = []
 month_profit_change = []
@@ -45,7 +45,6 @@ with open(csvpath) as csvfile:
 
         # Add month value to the month_count list
         # Get total months by getting the length of the month_count list
-        # UNSURE IF THIS IS THE BEST WAY TO DO THIS _but it works for now
         month_count.append(month)
         total_months = len(month_count)
         
@@ -62,18 +61,20 @@ with open(csvpath) as csvfile:
         else:
             month_profit_change.append(pl_change)
             pl_change = profit_loss - prev_row
-        #print(month_profit_change)
+        #print(month_profit_change) - life saving print statement!
     
         # This will store the value in the profit/loss column so that the next time we cycle through it can be used for the pl_change calculation
+        # This happens after the if statement so that it effects the next loop but not the current one
         prev_row = int(row[1])
-        
+
         
         # Calculate the greatest change in increase to the profit_loss column
         if pl_change > greatest_increase:
             greatest_increase = pl_change
+            # Grab the associated month when the greatest increase is found
             greatest_increase_month = row[0]
             
-        # Calculate the greatest change in decrease to the profit_loss column
+        # Same strategy as above, calculate the greatest change in decrease to the profit_loss column
         if pl_change < greatest_decrease:
             greatest_decrease = pl_change
             greatest_decrease_month = row[0]
@@ -82,21 +83,22 @@ with open(csvpath) as csvfile:
     # Get the average by dividing the monthly change numbers by the total months count
     # The len(month_profit_change) needs a -1 because there is no value change for the first month, meaning there is one less month in the average calculation
     average_change = sum(month_profit_change) / (len(month_profit_change) - 1)
+    # Round to two decimal places to match the assignments output
     average_change = str(round(average_change, 2))
     #print(sum(month_profit_change))
     #print((len(month_profit_change))) - This print statement saved me
 
 
 #-------------------------------------------------#
-# Print Statements (Test)
+# Print Statements
 # The text print statements and formatting are taken directly from the homework instructions, the values are dependant upon the code running successfully
 print(f"Financial Analysis")
 print(f"----------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: ${net_profit}")
 print(f"Average Change: ${average_change}")
-print(f"Greatest Increase in Profits: {greatest_increase_month} {greatest_increase}")
-print(f"Greatest Decrease in Profits: {greatest_decrease_month} {greatest_decrease}")
+print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
 
 #-------------------------------------------------#
 # Export text file of results to Analysis folder
